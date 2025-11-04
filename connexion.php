29fe51916adb20ad17b0c248afdbe6dd9e9e0374
ttvmgy <vermotgauchytitouan@gmail.com>
2025-11-04 16:46:19 +0100

    modification de connexion

--- connexion.php
+++ connexion.php
@@ -6,9 +6,6 @@ include('entete.php')
         if (!empty($_REQUEST['identifiant'])) {
             $identifiant = $_REQUEST['identifiant'];
             echo " <h2> Bonjour $identifiant, vous êtes connecté(e) ! </h2> ";
-        } else {
-            echo" <p> vous n'avez pas renseigné votre identifiant </p> ";
-        }
         ?>
      </section>
 
