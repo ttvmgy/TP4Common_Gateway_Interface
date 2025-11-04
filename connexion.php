862dcd9a4c4942eba37a55645f92930263fe4434
ttvmgy <vermotgauchytitouan@gmail.com>
2025-11-04 16:53:23 +0100

    resolution pb connexion

--- connexion.php
+++ connexion.php
@@ -1,14 +1,15 @@
 <?php
-include('entete.php')
+include('entete.php');
 ?>
     <section>
         <?php
         if (!empty($_REQUEST['identifiant'])) {
             $identifiant = $_REQUEST['identifiant'];
             echo " <h2> Bonjour $identifiant, vous êtes connecté(e) ! </h2> ";
+        }
         ?>
      </section>
 
 <?php
-include('pied_de_page.php')
+include('pied_de_page.php');
 ?>
