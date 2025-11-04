7a9fc12199f3fe8e60440faa8ba946f9249ebf88
ttvmgy <vermotgauchytitouan@gmail.com>
2025-11-04 17:10:50 +0100

    modif page connexion

--- formulaire.php
+++ formulaire.php
@@ -1,3 +1,23 @@
 <?php
+include('entete.php')
+?>
 
+        <form action='connexion.php'>
+            <ul>
+                <li>
+                    <label for="name">login</label>
+                    <input type="text" id="name" name="identifiant" />
+                </li>
+                <li>
+                    <label for="name">password</label>
+                    <input type="password" id="name" name="mot de passe" />
+                </li>
+                    <div class="button">
+                        <input type="submit" value="Envoyer le message">
+                    </div>
+            </ul>
+        </form>
+
+<?php
+include('pied_de_page.php')
 ?>
