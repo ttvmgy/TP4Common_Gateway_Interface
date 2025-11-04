831b0a71caf39f69c63c71a21af5608028dcdfa4
ttvmgy <vermotgauchytitouan@gmail.com>
2025-11-04 17:44:17 +0100

    //

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include('entete.php')
 ?>
 <h2>Acceuil</h2>
 <a href="formulaire.php" id="connexion">Connexion</a>
-<p>Veuiller vous connecter</p>
+<p id="#commentaire_page_acceuil">Veuiller vous connecter</p>
 <?php
 include('pied_de_page.php')
 ?>
