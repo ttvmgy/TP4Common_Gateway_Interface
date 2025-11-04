d4026a9403a41d40849a73ff81c7e9fa790bb039
ttvmgy <vermotgauchytitouan@gmail.com>
2025-11-04 16:09:26 +0100

    modif body

--- index.php
+++ index.php
@@ -3,21 +3,23 @@ include('entete.php')
 ?>
 <?php
 <html>
-<form action='connexion.php'>
-  <ul>
-    <li>
-      <label for="name">login;:</label>
-      <input type="text" id="name" name="identifiant" />
-    </li>
-    <li>
-      <label for="mail">password;:</label>
-      <input type="email" id="mail" name="mot de passe" />
-    </li>
-    <div class="button">
-        <button type="submit">Envoyer le message</button>
-    </div>
-  </ul>
-</form>
+    <body>
+        <form action='connexion.php'>
+            <ul>
+                <li>
+                    <label for="name">login;:</label>
+                    <input type="text" id="name" name="identifiant" />
+                </li>
+                <li>
+                    <label for="mail">password;:</label>
+                    <input type="email" id="mail" name="mot de passe" />
+                </li>
+                    <div class="button">
+                        <button type="submit">Envoyer le message</button>
+                    </div>
+            </ul>
+        </form>
+    </body>
 </html>
 ?>
 <?php
