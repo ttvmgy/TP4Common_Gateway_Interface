9da87b4e2473bdbff8efb94fb03f402091c65e95
ttvmgy <vermotgauchytitouan@gmail.com>
2025-11-04 15:54:23 +0100

    modification du form

--- index.php
+++ index.php
@@ -3,12 +3,15 @@ include('entete.php')
 ?>
 <?php
 
-<form action 'form.php'
-login :
-<input type "text" name = "identifiant">
-password :
-<input type "text" name = "mot de passe">
-
+<form action 'connexion.php'>
+    login :
+    <input type="text" name="identifiant">
+    password :
+    <input type="text" name="mot de passe">
+    <div class="button">
+        <button type="submit">Envoyer le message</button>
+    </div>
+</form>
 ?>
 <?php
 include('pied_de_page.php')
